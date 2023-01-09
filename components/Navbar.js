54c13aa6cc00e51043e087c871e3c2a90f0cd837
Emmanuel <emanuelanyigor@gmail.com>
2023-01-09 15:03:46 -0800

    header and navbar sections

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
+const Menu = () => {
+  return  (
+    <ul>
+      <li><Link href="/"> Home </Link></li>
+      <li><Link href="/about"> About </Link></li>
+      <li><Link href="/rules"> Rules </Link></li>
+      <li><Link href="/guardian"> Guaridan </Link></li>
+      <li><Link href="/how-it-works"> How It Works </Link></li>
+      <li><Link href="/divine-draw"> Divine Draw </Link></li>
+    </ul>
+  )
+}
+
 export default function Navbar() {
+  const [ toggle_menu, set_toggle_menu ] = useState(false);
   return (
-    <div className="navbar-container">
-        <ul>
-            <li><Link href="/"> Home </Link></li>
-            <li><Link href="/about"> About </Link></li>
-            <li><Link href="/rules"> Rules </Link></li>
-            <li><Link href="/guardian"> Guaridan </Link></li>
-            <li><Link href="/how-it-works"> How It Works </Link></li>
-            <li><Link href="/divine-draw"> Divine Draw </Link></li>
-        </ul>
-    </div>
+    <navbar className="app__navbar">
+      <div className="app__navbar-container">
+        <div className="app__navbar-container__logo">
+          <Link href="/"> LOGO </Link>
+        </div>
+        <div className="app__navbar-container__links">
+          <Menu />
+        </div>
+        <div className="app__navbar-container__button">
+          <Link href="/"> Play now </Link>
+        </div>
+      </div>
+    </navbar>
   )
 }
