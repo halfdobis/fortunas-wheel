fb02f5c9110ed5d36c81fa28c639152d239cebae
Emmanuel <emanuelanyigor@gmail.com>
2023-01-26 01:14:16 -0800

    navbar responsiveness

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,26 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
+import { RiCloseLine, RiMenu3Line } from 'react-icons/ri';
 import logo from '../assets/images/Fortuna-Logo.jpg';
 
-const Menu = () => {
+const Menu = ({ handle_click}) => {
   const { pathname } = useRouter();
   return  (
     <ul>
-      <li><Link href="/" className={`${pathname === "/" && "app__navbar-link__active"}`}> Home </Link></li>
-      <li><Link href="/about" className={`${pathname === "/about" && "app__navbar-link__active"}`}> About </Link></li>
-      <li><Link href="/rules" className={`${pathname === "/rules" && "app__navbar-link__active"}`}> Rules </Link></li>
-      <li><Link href="/story" className={`${pathname === "/story" && "app__navbar-link__active"}`}> Story </Link></li>
-      <li><Link href="/how-it-works"  className={`${pathname === "/how-it-works" && "app__navbar-link__active"}`}> How It Works </Link></li>
+      <li onClick={handle_click}><Link href="/" className={`${pathname === "/" && "app__navbar-link__active"}`}> Home </Link></li>
+      <li onClick={handle_click}><Link href="/about" className={`${pathname === "/about" && "app__navbar-link__active"}`}> About </Link></li>
+      <li onClick={handle_click}><Link href="/rules" className={`${pathname === "/rules" && "app__navbar-link__active"}`}> Rules </Link></li>
+      <li onClick={handle_click}><Link href="/story" className={`${pathname === "/story" && "app__navbar-link__active"}`}> Story </Link></li>
+      <li onClick={handle_click}><Link href="/how-it-works"  className={`${pathname === "/how-it-works" && "app__navbar-link__active"}`}> How It Works </Link></li>
     </ul>
   )
 }
 
 
 export default function Navbar() {
-
+  const [ toggle_menu, set_toggle_menu ] = useState(false);
   useEffect(()=> {
     const nav = document.querySelector(".app__navbar");
     window.addEventListener("scroll", (e)=> {
@@ -33,7 +34,7 @@ export default function Navbar() {
   },[])
 
   return (
-    <navbar className="app__navbar">
+    <nav className="app__navbar">
       <div className="app__navbar-container section__container">
         <div className="app__navbar-container__logo">
           <Link href="/">
@@ -43,7 +44,27 @@ export default function Navbar() {
         <div className="app__navbar-container__links">
           <Menu />
         </div>
+
+        <div className="app__navbar-container__btns">
+          { toggle_menu ? <RiCloseLine 
+              color='#fff'
+              size={27}
+              onClick={()=> set_toggle_menu(false)}
+          /> : <RiMenu3Line 
+              color='#fff'
+              size={27}
+              onClick={()=> set_toggle_menu(true)}
+          />}
+
+          { toggle_menu && (
+            <div className="app__navbar-mobile__menu scale-up-center">
+                <div className="app__navbar-mobile__menu-container">
+                  <Menu handle_click={()=> set_toggle_menu(false)} />
+                </div>
+            </div>
+          )}
+        </div>
       </div>
-    </navbar>
+    </nav>
   )
 }
