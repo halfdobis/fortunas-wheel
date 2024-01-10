c01afc81b08cc1e3dc2d3f28dace416fe4f69e7b
halfdobis <missdobis@gmail.com>
2024-01-10 21:13:47 +0100

    countdown timer & text edits

--- pages/divine-draw.js
+++ pages/divine-draw.js
@@ -1,21 +1,24 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
-import { BiArrowToRight, BiRightArrowAlt, BiLeftArrowAlt } from 'react-icons/bi';
-import { BsArrowDown } from 'react-icons/bs';
-
+import {
+  BiArrowToRight,
+  BiRightArrowAlt,
+  BiLeftArrowAlt,
+} from "react-icons/bi";
+import { BsArrowDown } from "react-icons/bs";
 
 export default function DivineDraw() {
   const ref = useRef(null);
-  const [ round, set_round ] = useState(250);
+  const [round, set_round] = useState(250);
   const handle_change = (e) => {
-    set_round(e.target.value)
-  }
+    set_round(e.target.value);
+  };
 
   const handle_click = () => {
     ref.current.scrollIntoView({
-      behaviour: "smooth"
-    })
-  }
+      behaviour: "smooth",
+    });
+  };
 
   const winning_balls = [
     { id: 1, number: 5 },
@@ -24,86 +27,94 @@ export default function DivineDraw() {
     { id: 4, number: 8 },
     { id: 5, number: 4 },
     { id: 6, number: 9 },
-  ]
+  ];
 
-  const top_winners = [
-    { id: 1, prize: '', match: '', }
-  ]
+  const top_winners = [{ id: 1, prize: "", match: "" }];
 
   // Set the date we're counting down to
-  const [ seconds, set_seconds ] = useState(0);
-  const [ days, set_days ] = useState(0);
-  const [ hours, set_hours ] = useState(0);
-  const [ minutes, set_minutes ] = useState(0);
-  var countDownDate = new Date("Dec 16, 2023 15:37:25").getTime();
+  const [seconds, set_seconds] = useState(0);
+  const [days, set_days] = useState(0);
+  const [hours, set_hours] = useState(0);
+  const [minutes, set_minutes] = useState(0);
+  var countDownDate = new Date("Feb 20, 2024 00:00:00").getTime();
 
-  
-  useEffect(()=> {
-      // Update the count down every 1 second
-      var x = setInterval(function() {
-          // Get today's date and time
-          var now = new Date().getTime();
-          // Find the distance between now and the count down date
-          var distance = countDownDate - now;              
-          // Time calculations for days, hours, minutes and seconds
-          set_days(Math.floor(distance / (1000 * 60 * 60 * 24)));
-          set_hours(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-          set_minutes(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
-          set_seconds(Math.floor((distance % (1000 * 60)) / (1000)));
-            
-      }, 1000);
-  },[])
+  useEffect(() => {
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+      // Get today's date and time
+      var now = new Date().getTime();
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+
+      if (distance <= 0) {
+        // If the countdown is finished, clear the interval
+        clearInterval(x);
+        // Optionally, you can set the values to zero or do any other actions
+        set_days(0);
+        set_hours(0);
+        set_minutes(0);
+        set_seconds(0);
+      } else {
+        // Time calculations for days, hours, minutes, and seconds
+        set_days(Math.floor(distance / (1000 * 60 * 60 * 24)));
+        set_hours(
+          Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        );
+        set_minutes(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
+        set_seconds(Math.floor((distance % (1000 * 60)) / 1000));
+      }
+    }, 1000);
+
+    // Cleanup function to clear the interval when the component is unmounted
+    return () => clearInterval(x);
+  }, []);
 
   return (
     <div className="app__divine-draw">
-      <div className='app__divine-draw__container'>
+      <div className="app__divine-draw__container">
         {/* Header */}
         <div className="app__divine-draw__header">
           <div className="app__divine-draw_overlay" />
           <div className="app__divine-draw__header-container">
-          <h4> The Divine Draw </h4>
-          <h2> Tickets on sale soon </h2>
-          <div className="countdown">
-            <div className="days time">
-              <div>
-                <h2> { days < 10 ? "0"+days : days } </h2>
+            <h4> The Divine Draw </h4>
+            <h2> Tickets on sale soon </h2>
+            <div className="countdown">
+              <div className="days time">
+                <div>
+                  <h2> {days < 10 ? "0" + days : days} </h2>
+                </div>
+                <p> Days </p>
               </div>
-              <p> Days </p>
-            </div>
-            <div className="hours time">
-              <div>
-                <h2> { hours < 10 ? "0"+hours : hours } </h2>
+              <div className="hours time">
+                <div>
+                  <h2> {hours < 10 ? "0" + hours : hours} </h2>
+                </div>
+                <p> hours </p>
               </div>
-              <p> hours </p>
-            </div>
-            <div className="minutes time">
-              <div>
-                <h2> { minutes < 10 ? "0"+minutes : minutes } </h2>
+              <div className="minutes time">
+                <div>
+                  <h2> {minutes < 10 ? "0" + minutes : minutes} </h2>
+                </div>
+                <p> minutes </p>
               </div>
-              <p> minutes </p>
-            </div>
-            <div className="seconds time">
-              <div>
-                <h2> { seconds < 10 ? "0"+seconds : seconds }</h2>
+              <div className="seconds time">
+                <div>
+                  <h2> {seconds < 10 ? "0" + seconds : seconds}</h2>
+                </div>
+                <p> seconds </p>
               </div>
-              <p> seconds </p>
             </div>
-          </div>
-          <button disabled={true}> Buy ticket </button>
+            <button disabled={true}> Buy ticket </button>
           </div>
 
           <div className="app__divine-draw__scroll">
-            <BsArrowDown 
-              size={32} 
-              color="#fff"
-              onClick={handle_click}
-            />
+            <BsArrowDown size={32} color="#fff" onClick={handle_click} />
           </div>
         </div>
 
         {/* HISTORY */}
         <div className="app__divine-draw__history section__container" ref={ref}>
-          { false ? (
+          {false ? (
             <div className="app__divine-draw__history-content">
               <div className="app__divine-draw__history-top">
                 <h2> Finished Rounds </h2>
@@ -113,9 +124,9 @@ export default function DivineDraw() {
                   <div className="ticket">
                     <p> winning numbers </p>
                     <div className="ticket__balls">
-                      { winning_balls.map(ball => (
+                      {winning_balls.map((ball) => (
                         <div className="balls" key={ball.id}>
-                          <h3>{ ball.number }</h3>
+                          <h3>{ball.number}</h3>
                         </div>
                       ))}
                     </div>
@@ -123,9 +134,9 @@ export default function DivineDraw() {
                 </div>
                 <div className="app__divine-draw__history-header">
                   <div className="app__divine-draw__history-header__left">
-                    <div className="app__divine-draw__history-round"> 
+                    <div className="app__divine-draw__history-round">
                       <span> Round </span>
-                      <input 
+                      <input
                         disabled={round >= 250 ? true : false}
                         min={1}
                         max={round}
@@ -133,7 +144,7 @@ export default function DivineDraw() {
                         type="number"
                       />
                     </div>
-                    <p> Drawn Jan 30, 2023, 5:00 PM </p>
+                    <p> Drawn Jan 30, 2024, 5:00 PM </p>
                   </div>
                   <div className="app__divine-draw__history-header__right">
                     <BiLeftArrowAlt />
@@ -146,7 +157,7 @@ export default function DivineDraw() {
                     <thead>
                       <tr>
                         <th> Wallet ID </th>
-                        <th className='ticket-box'> ticket </th>
+                        <th className="ticket-box"> ticket </th>
                         <th> Match </th>
                         <th> Prize </th>
                       </tr>
@@ -160,7 +171,7 @@ export default function DivineDraw() {
                 </div>
                 <div className="app__divine-draw__history-Footer"> Footer </div>
               </div>
-            </div> 
+            </div>
           ) : (
             <div className="app__divine-draw__history-empty">
               <h2> Finished Rounds </h2>
@@ -173,7 +184,7 @@ export default function DivineDraw() {
               </div>
             </div>
           )}
-          </div>
+        </div>
 
         {/* STEPS TO EARN */}
         <div className="app__divine-draw__steps-container section__container">
@@ -182,21 +193,33 @@ export default function DivineDraw() {
             <div className="app__divine-draw__step">
               <span> step 1</span>
               <h3> Buy Tickets </h3>
-              <p> Prices are set when the round starts, buy your ticket for just $1.00 and choose your ticket numbers.</p>
+              <p>
+                {" "}
+                Prices are set when the round starts, buy your ticket for just
+                $50.00. The more tickets you buy, the higher chances of winning.
+              </p>
             </div>
             <div className="app__divine-draw__step">
               <span> step 2</span>
               <h3> Wait for the Draw </h3>
-              <p> There is one draw every day alternating between 0 AM UTC and 12 PM UTC.</p>
+              <p>
+                {" "}
+                There is one draw every week alternating between Mondays, 0 AM
+                UTC through Fridays, 12 PM UTC.
+              </p>
             </div>
             <div className="app__divine-draw__step">
               <span> step 3</span>
               <h3> Check for Prizes </h3>
-              <p> Once the round’s over, come back to the page and check to see if you’ve won! </p>
+              <p>
+                {" "}
+                Once the round’s over, come back to the page and check to see if
+                you’ve won!{" "}
+              </p>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
